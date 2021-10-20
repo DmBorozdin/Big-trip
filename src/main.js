e1e7b8e475723ea0b0ef86649af5b71478f442ef
Бороздин Дмитрий <borozdin1@yandex.ru>
2021-10-20 22:15:01 +0700

    Добавлен компонент trip-info. Сделан рефакторинг.  Переименован renderElement в render.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { isDay1AfterDay2 } from './util.js';
 
 import MenuView from './view/menu.js';
+import TripInfoView from './view/trip-info.js';
 import RouteView from './view/route.js';
 import PriceView from './view/price.js';
 import FilterView from './view/filter.js';
@@ -11,7 +12,7 @@ import PointView from './view/point.js';
 import PointEditView from './view/point-edit.js';
 import { generatePoint } from './mock/point.js';
 // import { generateFilter } from './mock/filter.js';
-import { renderElement, RenderPosition } from './util.js';
+import { render, RenderPosition } from './util.js';
 
 const POINT_COUNT = 10;
 
@@ -25,19 +26,18 @@ const tripFilter = pageHeader.querySelector('.trip-controls__filters');
 const pageMain = document.querySelector('.page-main');
 const tripEvent = pageMain.querySelector('.trip-events');
 
-renderElement(tripNavigation, new MenuView().getElement(), RenderPosition.BEFOREEND);
-renderElement(tripMain, new RouteView(daySortPoints).getElement(), RenderPosition.AFTERBEGIN);
-const tripInfo = tripMain.querySelector('.trip-main__trip-info');
-renderElement(tripInfo, new PriceView(points).getElement(), RenderPosition.BEFOREEND);
-renderElement(tripFilter, new FilterView().getElement(), RenderPosition.BEFOREEND);
-renderElement(tripEvent, new SortView().getElement(), RenderPosition.BEFOREEND);
-renderElement(tripEvent, new TripListView().getElement(), RenderPosition.BEFOREEND);
+render(tripNavigation, new MenuView().getElement(), RenderPosition.BEFOREEND);
+const tripInfoComponent = new TripInfoView();
+render(tripMain, tripInfoComponent.getElement(), RenderPosition.AFTERBEGIN);
+render(tripInfoComponent.getElement(), new RouteView(daySortPoints).getElement(), RenderPosition.AFTERBEGIN);
+render(tripInfoComponent.getElement(), new PriceView(points).getElement(), RenderPosition.BEFOREEND);
+render(tripFilter, new FilterView().getElement(), RenderPosition.BEFOREEND);
+render(tripEvent, new SortView().getElement(), RenderPosition.BEFOREEND);
+const tripListComponent = new TripListView();
+render(tripEvent, tripListComponent.getElement(), RenderPosition.BEFOREEND);
 
-const tripEventList = pageMain.querySelector('.trip-events__list');
-
-renderElement(tripEventList, new PointEditView(daySortPoints[0]).getElement(), RenderPosition.BEFOREEND);
+render(tripListComponent.getElement(), new PointEditView(daySortPoints[0]).getElement(), RenderPosition.BEFOREEND);
 // renderTemplate(tripEventList, createNewPoint(), 'beforeend');
-
 for (let i = 1; i < POINT_COUNT; i ++) {
-  renderElement(tripEventList, new PointView(daySortPoints[i]).getElement(), RenderPosition.BEFOREEND);
+  render(tripListComponent.getElement(), new PointView(daySortPoints[i]).getElement(), RenderPosition.BEFOREEND);
 }
--- src/view/route.js
+++ src/view/route.js
@@ -8,13 +8,11 @@ const createRouteTemplate = (points) => {
   const lastDate = getDateInFormatDM(points[points.length - 1].dateFrom);
   const towns = Array.from(new Set(points.map((point) => point.town)));
 
-  return `<section class="trip-main__trip-info  trip-info">
-<div class="trip-info__main">
-  <h1 class="trip-info__title"> ${towns.length <= TOWNS_COUNT ? towns.map((town) => town).join(' &mdash; ') : `${towns[0]} &mdash; ... &mdash; ${towns[towns.length-1]}`}</h1>
+  return `<div class="trip-info__main">
+    <h1 class="trip-info__title"> ${towns.length <= TOWNS_COUNT ? towns.map((town) => town).join(' &mdash; ') : `${towns[0]} &mdash; ... &mdash; ${towns[towns.length-1]}`}</h1>
 
-  <p class="trip-info__dates">${firstDate}&nbsp;&mdash;&nbsp;${lastDate}</p>
-</div>
-</section>`;
+    <p class="trip-info__dates">${firstDate}&nbsp;&mdash;&nbsp;${lastDate}</p>
+  </div>`;
 };
 
 export default class Route {
