f3b8fb2734c56816681e4246045b061f45255dc0
Дмитрий Бороздин <borozdin1@yandex.ru>
2021-10-21 11:15:36 +0000

    Добавлено закрытие формы редактирования по клавише ESC. Добавлен компонент с приглашением добавить первую точку если список пуст. Сделан рефакторинг кода.

--- src/main.js
+++ src/main.js
@@ -11,10 +11,20 @@ import PointView from './view/point.js';
 // import { createNewPoint } from './view/new-point.js';
 import PointEditView from './view/point-edit.js';
 import { generatePoint } from './mock/point.js';
+import EmptyListView from './view/list-empty.js';
 // import { generateFilter } from './mock/filter.js';
 import { render, RenderPosition } from './util.js';
 
 const POINT_COUNT = 10;
+
+const points = new Array(POINT_COUNT).fill(null).map(generatePoint);
+const daySortPoints = points.slice().sort((point1, point2) =>  isDay1AfterDay2(point1.dateFrom, point2.dateFrom));
+
+const pageHeader = document.querySelector('.page-header');
+const tripMain = pageHeader.querySelector('.trip-main');
+const tripNavigation = pageHeader.querySelector('.trip-controls__navigation');
+const tripFilter = pageHeader.querySelector('.trip-controls__filters');
+const tripEvent = document.querySelector('.trip-events');
 
 const renderPoint = (tripListElement, point) => {
   const pointComponent = new PointView(point);
@@ -28,43 +38,58 @@ const renderPoint = (tripListElement, point) => {
     tripListElement.replaceChild(pointComponent.getElement(), pointEditComponent.getElement());
   };
 
+  const onEscKeyDown = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      replaceFormToPoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    }
+  };
+
   pointComponent.getElement().querySelector('.event__rollup-btn').addEventListener('click', () => {
     replacePointToForm();
+    document.addEventListener('keydown', onEscKeyDown);
   });
 
   pointEditComponent.getElement().querySelector('form').addEventListener('submit', (evt) => {
     evt.preventDefault();
     replaceFormToPoint();
+    document.removeEventListener('keydown', onEscKeyDown);
   });
 
   pointEditComponent.getElement().querySelector('.event__rollup-btn').addEventListener('click', () => {
     replaceFormToPoint();
+    document.removeEventListener('keydown', onEscKeyDown);
   });
 
   render(tripListElement, pointComponent.getElement(), RenderPosition.BEFOREEND);
 };
 
-const points = new Array(POINT_COUNT).fill(null).map(generatePoint);
-const daySortPoints = points.slice().sort((point1, point2) =>  isDay1AfterDay2(point1.dateFrom, point2.dateFrom));
+const renderTripInfo = (tripInfoContainer, tripInfoPoints) => {
+  if (tripInfoPoints.length !== 0) {
+    const tripInfoComponent = new TripInfoView();
+    render(tripInfoContainer, tripInfoComponent.getElement(), RenderPosition.AFTERBEGIN);
+    render(tripInfoComponent.getElement(), new RouteView(daySortPoints).getElement(), RenderPosition.AFTERBEGIN);
+    render(tripInfoComponent.getElement(), new PriceView(points).getElement(), RenderPosition.BEFOREEND);
+  }
+};
 
-const pageHeader = document.querySelector('.page-header');
-const tripMain = pageHeader.querySelector('.trip-main');
-const tripNavigation = pageHeader.querySelector('.trip-controls__navigation');
-const tripFilter = pageHeader.querySelector('.trip-controls__filters');
-const pageMain = document.querySelector('.page-main');
-const tripEvent = pageMain.querySelector('.trip-events');
+const renderList = (tripListContainer, listPoints) => {
+  if (listPoints.length === 0) {
+    render(tripListContainer, new EmptyListView().getElement(), RenderPosition.BEFOREEND);
+  } else {
+    render(tripListContainer, new SortView().getElement(), RenderPosition.BEFOREEND);
+    const tripListComponent = new TripListView();
+    render(tripListContainer, tripListComponent.getElement(), RenderPosition.BEFOREEND);
+
+    // renderTemplate(tripEventList, createNewPoint(), 'beforeend');
+    for (let i = 0; i < POINT_COUNT; i ++) {
+      renderPoint(tripListComponent.getElement(), listPoints[i]);
+    }
+  }
+};
 
 render(tripNavigation, new MenuView().getElement(), RenderPosition.BEFOREEND);
-const tripInfoComponent = new TripInfoView();
-render(tripMain, tripInfoComponent.getElement(), RenderPosition.AFTERBEGIN);
-render(tripInfoComponent.getElement(), new RouteView(daySortPoints).getElement(), RenderPosition.AFTERBEGIN);
-render(tripInfoComponent.getElement(), new PriceView(points).getElement(), RenderPosition.BEFOREEND);
 render(tripFilter, new FilterView().getElement(), RenderPosition.BEFOREEND);
-render(tripEvent, new SortView().getElement(), RenderPosition.BEFOREEND);
-const tripListComponent = new TripListView();
-render(tripEvent, tripListComponent.getElement(), RenderPosition.BEFOREEND);
-
-// renderTemplate(tripEventList, createNewPoint(), 'beforeend');
-for (let i = 0; i < POINT_COUNT; i ++) {
-  renderPoint(tripListComponent.getElement(), daySortPoints[i]);
-}
+renderTripInfo(tripMain, points);
+renderList(tripEvent, daySortPoints);
---
+++ src/view/list-empty.js
@@ -0,0 +1,25 @@
+import {createElement} from '../util.js';
+
+const createEmptyListTemplate = () => '<p class="trip-events__msg">Click New Event to create your first point</p>';
+
+export default class EmptyList {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createEmptyListTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
